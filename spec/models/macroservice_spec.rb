9fe765e86f44d7a13d113973b64256a3c473c78a
Philipe <philipe-leandro@hotmail.com>
2022-03-04 17:21:51 -0300

    Add shulda test for category and macrosservice relationship

--- spec/models/macroservice_spec.rb
+++ spec/models/macroservice_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Macroservice, type: :model do
-  # describe 'has_many association' do
-  #   it { expected has_many(:category)}
-  # end
+  describe 'has_many association' do
+    it { should have_many(:categorys)}
+  end
 end
